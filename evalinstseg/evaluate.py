dc81b94ee58af9683743adbe23c4f4397805b3f1
maisli <lisa.mais@gmx.net>
2024-02-05 08:07:46 -0500

    add average sets

--- evalinstseg/evaluate.py
+++ evalinstseg/evaluate.py
@@ -505,6 +505,51 @@ def average_flylight_score_over_instances(samples_foldn, result):
     return avS, per_instance_counts
 
 
+def average_sets(acc_a, dict_a, acc_b, dict_b):
+    threshs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    acc = np.mean([acc_a, acc_b])
+    print(acc_a, acc_b, acc)
+    fscore = np.mean([dict_a["general"]["avFscore"],
+        dict_b["general"]["avFscore"]])
+    gt_covs = list(dict_a["gt_covs"]) + list(dict_b["gt_covs"])
+    per_instance_counts = {}
+    per_instance_counts["general"] = {
+        "Num GT": dict_a["general"]["Num GT"] + dict_b["general"]["Num GT"],
+        "Num Pred": dict_a["general"]["Num Pred"] + dict_b["general"]["Num Pred"],
+        "avg_gt_skel_coverage": np.mean([
+            dict_a["general"]["avg_gt_skel_coverage"],
+            dict_b["general"]["avg_gt_skel_coverage"]]),
+        "avg_f1_cov_score": acc,
+        "avFscore": fscore
+    }
+    per_instance_counts["confusion_matrix"] = {"avFscore": fscore}
+    per_instance_counts["gt_covs"] = gt_covs
+    per_instance_counts["false_split"] = dict_a["false_split"] + dict_b["false_split"]
+    per_instance_counts["false_merge"] = dict_a["false_merge"] + dict_b["false_merge"]
+    for i, thresh in enumerate(threshs):
+        cm_a = dict_a["confusion_matrix"]["th_" + str(thresh).replace(".", "_")]
+        cm_b = dict_b["confusion_matrix"]["th_" + str(thresh).replace(".", "_")]
+        per_instance_counts["confusion_matrix"][
+            "th_" + str(thresh).replace(".", "_")] = {
+            "fscore": np.mean([cm_a["fscore"],cm_b["fscore"]])}
+        if thresh == 0.5:
+            per_instance_counts["confusion_matrix"]["th_0_5"]["AP_TP"] = \
+                    cm_a["AP_TP"] + cm_b["AP_TP"]
+            per_instance_counts["confusion_matrix"]["th_0_5"]["AP_FP"] = \
+                    cm_a["AP_FP"] + cm_b["AP_FP"]
+            per_instance_counts["confusion_matrix"]["th_0_5"]["AP_FN"] = \
+                    cm_a["AP_FN"] + cm_b["AP_FN"]
+            per_instance_counts["confusion_matrix"]["th_0_5"][
+                "false_split"] = cm_a["false_split"] + cm_b["false_split"]
+            per_instance_counts["confusion_matrix"]["th_0_5"][
+                "false_merge"] = cm_a["false_merge"] + cm_b["false_merge"]
+            per_instance_counts["confusion_matrix"]["th_0_5"][
+                "avg_tp_skel_coverage"] = np.mean(
+                        [cm_a["avg_tp_skel_coverage"],
+                            cm_b["avg_tp_skel_coverage"]])
+    return acc, per_instance_counts
+
+
 def main():
     """main entry point if called from command line
 
@@ -524,13 +569,15 @@ def main():
             help='name result hdf/zarr key')
     parser.add_argument('--gt_key', type=str,
             help='name ground truth hdf/zarr key')
-    parser.add_argument('--out_dir', type=str,
+    parser.add_argument('--out_dir', nargs="+", type=str,
             help='output directory', required=True)
     parser.add_argument('--suffix', type=str,
             help='suffix (deprecated)', default='')
     parser.add_argument('--ndim', type=int,
             default=3,
             help='number of spatial dimensions')
+    parser.add_argument('--summary_out_dir', type=str, default=None,
+            help='output directory for summary')
     # metrics definitions
     parser.add_argument('--localization_criterion', type=str,
             help='localization_criterion', default='iou',
@@ -561,7 +608,7 @@ def main():
     parser.add_argument('--partly', action='store_true',
             default=False,
             help='if ground truth is only labeled partly')
-    parser.add_argument('--partly_list', nargs="+", type=bool, default=None,
+    parser.add_argument('--partly_list', nargs="+", type=int, default=None,
             help='use partly_list if you have a list of completely and partly inputs')
     parser.add_argument('--foreground_only', action='store_true',
             default=False,
@@ -612,11 +659,26 @@ def main():
             assert len(args.partly_list) == len(args.res_file), \
                     "Please check, not the same number of result files "\
                     "and partly_list values"
-            partly_list = args.partly_list
+            partly_list = np.array(args.partly_list, dtype=bool)
         else:
             partly_list = [args.partly] * len(args.res_file)
     else:
         partly_list = [args.partly]
+
+    # check out_dir
+    if len(args.res_file) > 1:
+        if len(args.out_dir) > 1:
+            assert len(args.res_file) == len(args.out_dir), \
+                    "Please check, number of input files and output folders should correspond"
+            outdir_list = args.out_dir
+        else:
+            outdir_list = args.out_dir * len(args.res_file)
+    else:
+        assert len(args.out_dir) == 1, "Please check number of output directories"
+        outdir_list = args.out_dir
+    # check output dir for summary
+    if args.summary_out_dir is None:
+        args.summary_out_dir = args.out_dir[0]
 
     if args.app is not None:
         if args.app == "flylight":
@@ -653,10 +715,11 @@ def main():
                     "confusion_matrix.th_0_5.false_merge",
                     "confusion_matrix.th_0_5.avg_tp_skel_coverage"
                     ]
+            args.visualize_type = "neuron"
 
     samples = []
     metric_dicts = []
-    for res_file, gt_file, partly in zip(args.res_file, args.gt_file, partly_list):
+    for res_file, gt_file, partly, out_dir in zip(args.res_file, args.gt_file, partly_list, outdir_list):
         sample_name = os.path.basename(res_file).split(".")[0]
         print("sample_name: ", sample_name)
         print("res_file: ", res_file)
@@ -672,7 +735,7 @@ def main():
                 res_file,
                 gt_file,
                 args.ndim,
-                args.out_dir,
+                out_dir,
                 res_key=args.res_key,
                 gt_key=args.gt_key,
                 suffix=args.suffix,
@@ -700,11 +763,22 @@ def main():
         if metric_dict is None:
             continue
         metrics_full[sample] = metric_dict
+    if len(np.unique(partly_list)) > 1:
+        print("averaging for combined")
+        # get average over instances for completely
+        samples = np.array(samples)
+        acc_cpt, acc_inst_cpt = average_flylight_score_over_instances(
+                samples[partly_list==False], metrics_full)
+        acc_prt, acc_inst_prt = average_flylight_score_over_instances(
+                samples[partly_list==True], metrics_full)
+        acc, acc_all_instances = average_sets(acc_cpt, acc_inst_cpt,
+                acc_prt, acc_inst_prt)
 
-    acc, acc_all_instances = average_flylight_score_over_instances(
-        samples, metrics_full)
+    else:
+        acc, acc_all_instances = average_flylight_score_over_instances(
+            samples, metrics_full)
     summarize_metric_dict(metric_dicts, samples, args.summary,
-                          os.path.join(args.out_dir, "summary.csv"),
+                          os.path.join(args.summary_out_dir, "summary.csv"),
                           agg_inst_dict=acc_all_instances
                           )
 
